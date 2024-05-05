e579070b3ed1e3e33959e2cc28acca6c805ef3d3
AlanCruz10 <exalan30@gmail.com>
2024-05-04 19:04:04 -0600

    [ADD] new message

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ app.get('/', (req, res) => {
   res.send('¡Hola, mundo!');
 });
 
+app.get('/api', (req, res) => {
+  res.send('nueva url');
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`);
