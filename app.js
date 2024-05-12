ae06f9fedac26dd09a808f16dac15e355d85fcd6
AlanCruz10 <exalan30@gmail.com>
2024-05-11 22:57:58 -0600

    [UPDATE] app

--- app.js
+++ app.js
@@ -34,6 +34,10 @@ app.get('/api/v1/users/:user', (req, res) => {
   }
 });
 
+app.get('/api', (req, res) => {
+    res.status(200).json({ message: 'Funciona' });
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`);
