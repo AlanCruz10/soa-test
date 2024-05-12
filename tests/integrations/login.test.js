c199e865e4666cbd3e06022f0bedb9704e173cce
AlanCruz10 <exalan30@gmail.com>
2024-05-12 00:52:24 -0600

    [UPDATE] test2

--- tests/integrations/login.test.js
+++ tests/integrations/login.test.js
@@ -5,7 +5,7 @@ describe('Login test', () => {
     it('debería autenticar con credenciales válidas', (done) => {
         request(app)
             .post('/api/v1/login')
-            .send({ username: 'user_i', password: 'pass_i' })
+            .send({ username: 'user_in', password: 'pass_in' })
             .end((err, res) => {
                 if (err) return done(err);
                 if (res.status !== 200) {
