be936ed6aa3c8576a6aca28b73624a2de9f4e616
AlanCruz10 <exalan30@gmail.com>
2024-06-04 14:08:00 -0600

    [UPDATE] pipeline 59

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.use('/api/v1', studentRoutes);
 // });
 
 app.get('/api/v1/welcome', (req, res) => {
-    res.status(200).json({ message: 'Welcome to this Api' });
+    res.status(200).json({ message: 'Welcome to this Api ya' });
 });
 
 const PORT = process.env.PORT || 3000;
